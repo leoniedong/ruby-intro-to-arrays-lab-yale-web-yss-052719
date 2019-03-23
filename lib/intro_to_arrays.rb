fdef6f668986ee343b5098586819866041fe8ba0
Leonie Dong <leoniedong@hotmail.com>
2019-03-23 14:11:42 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  return ["one", "two"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array[0]
+end
+
+def last_element_with_array_methods(array)
+  return array[-1]
+end
+
+def length_of_array(array)
+  return array.length
+end
