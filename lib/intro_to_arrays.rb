311a8faa65b8413d685cbec6961e5ed7ba4f1c98
Leonie Dong <leoniedong@hotmail.com>
2019-03-23 14:09:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,27 @@
+def  instantiate_new_array
+  []
+end
+
+def  array_with_two_elements
+  return [element_one, element_two]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array[0]
+end
+
+def last_element_with_array_methods(array)
+  return array[-1]
+end
